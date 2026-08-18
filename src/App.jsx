b45ebd93d4bc131ee2b40e51047cb183a8f9973f
Olya <belenkod55@gmail.com>
2026-08-18 21:46:05 +0300

    prepare app for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
-import './App.css'
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>logo</h1>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -18,18 +18,18 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'portrait-primary',
 
-        icons: [
-          {
-            src: '/icon-192.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml',
-          },
-          {
-            src: '/icon-512.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml',
-          },
-        ],
+  icons: [
+  {
+    src: '/icon.svg',
+    sizes: '192x192',
+    type: 'image/svg+xml',
+  },
+  {
+    src: '/icon.svg',
+    sizes: '512x512',
+    type: 'image/svg+xml',
+  },
+],
       },
     }),
   ],
